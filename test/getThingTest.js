e385928979e33b6dff2c8ee92f890e5b7d09aedf
JoonHo Son <jhson@nagesoft.com>
2015-12-23 11:27:03 +0900

    * Test case 작성.

--- test/getThingTest.js
+++ test/getThingTest.js
@@ -1,3 +1,25 @@
 /**
  * Created by JoonHo Son on 2015. 11. 24..
  */
+
+'use strict';
+
+var http = require('http');
+var wot = require('../wot');
+var express = require('express');
+var app = express();
+var server = http.createServer(app);
+var netProfile;
+var WPx_THING_ID = 'init_test_thing_id';
+
+describe('Array', function () {
+    describe('Wot.js#init', function () {
+        var options = {
+            websocketTopic: 'sensorData',
+            reportInterval: netProfile && netProfile.reportingPeriod
+        };
+
+        wot.init(WPx_THING_ID, server, options, function () {
+        });
+    });
+});
--- test/setActuatorTest.js
+++ test/setActuatorTest.js
@@ -1,3 +1,27 @@
 /**
  * Created by JoonHo Son on 2015. 11. 24..
  */
+
+'use strict';
+
+var http = require('http');
+var wot = require('../wot');
+var express = require('express');
+var app = express();
+var server = http.createServer(app);
+var netProfile;
+var WPx_THING_ID = 'init_test_thing_id';
+
+describe('Array', function () {
+    describe('Wot.js#init', function () {
+        var options = {
+            websocketTopic: 'sensorData',
+            reportInterval: netProfile && netProfile.reportingPeriod
+        };
+
+        wot.init(WPx_THING_ID, server, options, function () {
+            var url = 'sensorjs:///gpio/18/rgbLed/rgbLed-18';
+            wot.setActuator(url, {'command': 'powerOn'}, null);
+        });
+    });
+});
